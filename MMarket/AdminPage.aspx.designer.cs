9c2dbb0ccea4e65febf28e00a7d61dc061a68946
Adel <adel.othman@live.com>
2017-09-15 20:31:02 +0200

    Dodan footer

--- MMarket/AdminPage.aspx.designer.cs
+++ MMarket/AdminPage.aspx.designer.cs
@@ -31,15 +31,6 @@ namespace MMarket {
         protected global::System.Web.UI.WebControls.Button Button1;
         
         /// <summary>
-        /// lblError control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblError;
-        
-        /// <summary>
         /// DetailsView1 control.
         /// </summary>
         /// <remarks>
