750e2647cbac9c0f05f8bcf2680a6455456016df
ivaana.perko <ivaana.perko@gmail.com>
2017-09-16 20:55:30 +0200

    nav

--- MMarket/Pica.aspx.designer.cs
+++ MMarket/Pica.aspx.designer.cs
@@ -13,6 +13,15 @@ namespace MMarket {
     public partial class Pica {
         
         /// <summary>
+        /// Image3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image Image3;
+        
+        /// <summary>
         /// Panel1 control.
         /// </summary>
         /// <remarks>
