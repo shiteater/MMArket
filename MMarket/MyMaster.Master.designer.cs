a17c4449996f24ca905636a7c41a39a1a2fc56af
Adel <adel.othman@live.com>
2017-09-16 23:16:45 +0200

    donji master centriran

--- MMarket/MyMaster.Master.designer.cs
+++ MMarket/MyMaster.Master.designer.cs
@@ -229,6 +229,15 @@ namespace MMarket {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
         
         /// <summary>
+        /// Panel1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel Panel1;
+        
+        /// <summary>
         /// ContentPlaceHolder2 control.
         /// </summary>
         /// <remarks>
