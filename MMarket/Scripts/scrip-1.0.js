a8ababd0e4942ce906f573812bed17346facf704
Adel <adel.othman@live.com>
2017-09-17 11:50:50 +0200

    Nema vise slide s jedan ili dva proizvda

--- MMarket/Scripts/scrip-1.0.js
+++ MMarket/Scripts/scrip-1.0.js
@@ -1,14 +1,18 @@
 ﻿$(window).scroll(function () {
     var Height1 = document.getElementById("content_div").clientHeight;
-    //var Height2 = document.getElementById("top_nav").clientHeight;
+    var Height2 = document.getElementById("nav_sec").clientHeight;
     if ($(window).scrollTop() > Height1) {
         $('#nav_sec').addClass('navbarsec-fixed');
-        $('#conDiv2').addClass('transCon');
-        $('#myFooter').addClass('transCon');
+        //$('#conDiv2').addClass('transCon2');
+        $('#conDiv2').css("transform", "translateY(" + Height2 + "px)");
+        //$('#myFooter').addClass('transCon');
+        $('#myFooter').css("transform", "translateY(" + Height2 + "px)");
     }
     if ($(window).scrollTop() < Height1 + 1) {
         $('#nav_sec').removeClass('navbarsec-fixed');
-        $('#conDiv2').removeClass('transCon');
-        $('#myFooter').removeClass('transCon');
+        //$('#conDiv2').removeClass('transCon');
+        $('#conDiv2').css("transform", "translateY(0px)");
+        //$('#myFooter').removeClass('transCon');
+        $('#myFooter').css("transform", "translateY(0px)");
     }
 });
