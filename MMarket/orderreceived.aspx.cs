ed6d600c0ab5f7ece23e5808b8ae1b031258c8a8
Adel <adel.othman@live.com>
2017-09-29 19:46:18 +0200

    com

--- MMarket/orderreceived.aspx.cs
+++ MMarket/orderreceived.aspx.cs
@@ -218,18 +218,8 @@ namespace MMarket
 
             Response.ContentType = "application/pdf";
             Response.AddHeader("content-disposition", "attachment;filename=Narudzbe" + idNarudzba + ".pdf");
-            Response.Cache.SetCacheability(HttpCacheability.NoCache);
-            StringWriter sw = new StringWriter();
-            HtmlTextWriter hw = new HtmlTextWriter(sw);
-            Panel1.RenderControl(hw);
-            StringReader sr = new StringReader(sw.ToString());
-            Document pdfDoc = new Document(PageSize.A4, 10f, 10f, 100f, 0f);
-            HTMLWorker htmlparser = new HTMLWorker(pdfDoc);
-            PdfWriter.GetInstance(pdfDoc, Response.OutputStream);
-            pdfDoc.Open();
-            htmlparser.Parse(sr);
-            pdfDoc.Close();
-            Response.Write(pdfDoc);
+            Response.TransmitFile(Server.MapPath("~/Narudzbe/Narudzba" + idNarudzba + ".pdf"));
+            Response.End();
         }
     }
 }
